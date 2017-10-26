228e42d08280369a9a5eab2dac9d938242804c78
heckenna <heckenna@rose-hulman.edu>
2017-10-26 14:19:16 -0400

    kfhsadhfkdsa

--- sandbox/Noah/motors/m1_drive_timed.py
+++ sandbox/Noah/motors/m1_drive_timed.py
@@ -106,6 +106,7 @@ def main():
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
 
+main()
 # DONE: 4. Change the input questions from:
 #   Enter a speed for the left motor (0 to 900 dps):
 #   Enter a speed for the right motor (0 to 900 dps):
