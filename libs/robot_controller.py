810aecb08b4c5bd5edb4db1e57ee3126bbada37f
wangj14 <wangj14@rose-hulman.edu>
2017-10-26 15:13:55 -0400

    Junheng

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -34,3 +34,17 @@ class Snatch3r(object):
         self.left_motor.run_to_rel_pos(position_sp = inches_target * 90, speed_sp = speed_deg_per_second)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
+    def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+        # Check that the motors are actually connected
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
+        self.right_motor.run_to_rel_pos(position_sp = degrees_to_turn * 180, speed_sp = turn_speed_sp)
+        self.left_motor.run_to_rel_pos(position_sp = degrees_to_turn * -180, speed_sp = turn_speed_sp)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
