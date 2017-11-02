2b44e4fdcec1fa775aa50ffd3830e809383d8818
wangj14 <wangj14@rose-hulman.edu>
2017-11-02 15:10:56 -0400

    Junheng

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -140,8 +140,8 @@ class Snatch3r(object):
         assert self.left_motor.connected
         assert self.right_motor.connected
 
-        self.right_motor.stop('brake')
-        self.left_motor.stop('brake')
+        self.right_motor.stop()
+        self.left_motor.stop()
 
 
     def left_motor_forward(self):
