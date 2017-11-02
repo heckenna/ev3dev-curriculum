d62266190139506f5822cb896a6ac768e9e4d231
wangj14 <wangj14@rose-hulman.edu>
2017-11-02 15:13:43 -0400

    Junheng

--- sandbox/Junheng/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/Junheng/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -89,7 +89,7 @@ def drive_to_color(button_state, robot, color_to_seek):
     if button_state:
         ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
 
-        # TODO: 3. Implement the task as stated in this module's initial comment block
+        # DONE: 3. Implement the task as stated in this module's initial comment block
         # It is recommended that you add to your Snatch3r class's constructor the color_sensor, as shown
         #   self.color_sensor = ev3.ColorSensor()
         #   assert self.color_sensor
