d69575239c44f679d2b97e7e84bfecd84cc286d8
heckenna <heckenna@rose-hulman.edu>
2017-11-05 20:38:28 -0500

    kfhsadhfkdsa

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -32,6 +32,8 @@ class Snatch3r(object):
         self.color_sensor = ev3.ColorSensor()
         self.ir_sensor = ev3.InfraredSensor()
         self.pixy = ev3.Sensor(driver_name="pixt-lego")
+
+        assert ev3.InfraredSensor()
         assert ev3.Sensor(driver_name="pixt-lego")
 
 
