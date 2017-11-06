d72619f29d82f06c81821b6dbf1284fcd7912d8f
wangj14 <wangj14@rose-hulman.edu>
2017-11-06 15:19:29 -0500

    Junheng

--- projects/Junheng/project_ev3.py
+++ projects/Junheng/project_ev3.py
@@ -7,7 +7,9 @@ import mqtt_remote_method_calls as com
 import tkinter
 from tkinter import ttk
 
+
 def main():
-
-
-def
+    robot = robo.Snatch3r()
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+    robot.loop_forever()
