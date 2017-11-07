fb2c641d8e1eb8749b329e40061776aa04ab7277
wangj14 <wangj14@rose-hulman.edu>
2017-11-06 23:50:56 -0500

    Junheng

--- projects/Junheng/project_2.py
+++ projects/Junheng/project_2.py
@@ -1,12 +1,7 @@
 
 import time
-import random
-import traceback
 import ev3dev.ev3 as ev3
 import robot_controller as robo
-import mqtt_remote_method_calls as com
-import tkinter
-from tkinter import ttk
 
 
 def main():
@@ -22,14 +17,25 @@ def main():
             break
         if robot.color_sensor.color == ev3.ColorSensor.COLOR_BLUE:
             robot.drive_stop()
-            robot.turn_degrees(180, 100)
+            robot.turn_degrees(190, 100)
             speed = -1*speed
+            robot.drive_forward(speed, speed)
+            time.sleep(0.2)
         if robot.color_sensor.color == ev3.ColorSensor.COLOR_RED:
             robot.drive_stop()
-            robot.arm_calibration()
+            robot.arm_up()
+            robot.drive_forward(speed, speed)
+            time.sleep(0.2)
+        if robot.color_sensor.color == ev3.ColorSensor.COLOR_WHITE:
+            robot.drive_stop()
+            robot.arm_down()
+            robot.drive_forward(speed, speed)
+            time.sleep(0.2)
         if robot.color_sensor.color == ev3.ColorSensor.COLOR_BLACK:
             robot.drive_stop()
             ev3.Sound.play("/home/robot/csse120/assets/sounds/awesome_pcm.wav").wait()
+            robot.drive_forward(speed, speed)
+            time.sleep(0.2)
         time.sleep(0.1)
 
 
