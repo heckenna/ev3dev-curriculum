196ccbfb5dd9c7c04dcaaedbce6d102112f62d0b
wangj14 <wangj14@rose-hulman.edu>
2017-11-09 14:55:27 -0500

    Junheng

--- projects/Junheng/project_ev3.py
+++ projects/Junheng/project_ev3.py
@@ -9,61 +9,61 @@ def main():
     robot = robo.Snatch3r()
     mqtt_client = com.MqttClient(robot)
     mqtt_client.connect_to_pc()
-
-    rc1 = ev3.RemoteControl(channel=1)
-    rc2 = ev3.RemoteControl(channel=2)
-    assert rc1.connected
-    assert rc2.connected
-
-    rc1.on_red_up = lambda state: left_motor_forward_button(state, robot)
-    rc1.on_red_down = lambda state: left_motor_backward_button(state, robot)
-    rc1.on_blue_up = lambda state: right_motor_forward_button(state, robot)
-    rc1.on_blue_down = lambda state: right_motor_backward_button(state, robot)
-
-    rc2.on_red_up = lambda state: handle_arm_up_button(state, robot)
-    rc2.on_red_down = lambda state: handle_arm_down_button(state, robot)
-    rc2.on_blue_up = lambda state: handle_calibrate_button(state, robot)
-    rc2.on_blue_down = lambda state: handle_calibrate_button(state, robot)
-
-    while True:
-        rc1.process()
-        rc2.process()
-        time.sleep(0.1)
     robot.loop_forever()
 
-
-def left_motor_forward_button(button_state, robot):
-    if button_state:
-        robot.left_motor_forward()
-    else:
-        robot.left_motor.stop()
-
-def left_motor_backward_button(button_state, robot):
-    if button_state:
-        robot.left_motor_backward()
-    else:
-        robot.left_motor.stop()
-
-def right_motor_forward_button(button_state, robot):
-    if button_state:
-        robot.right_motor_forward()
-    else:
-        robot.right_motor.stop()
-
-def right_motor_backward_button(button_state, robot):
-    if button_state:
-        robot.right_motor_backward()
-    else:
-        robot.right_motor.stop()
-
-def handle_arm_up_button(button_state, robot):
-    if button_state:
-        robot.arm_up()
-
-def handle_arm_down_button(button_state, robot):
-    if button_state:
-        robot.arm_down()
-
-def handle_calibrate_button(button_state, robot):
-    if button_state:
-        robot.arm_calibration()
+#     rc1 = ev3.RemoteControl(channel=1)
+#     rc2 = ev3.RemoteControl(channel=2)
+#     assert rc1.connected
+#     assert rc2.connected
+#
+#     rc1.on_red_up = lambda state: left_motor_forward_button(state, robot)
+#     rc1.on_red_down = lambda state: left_motor_backward_button(state, robot)
+#     rc1.on_blue_up = lambda state: right_motor_forward_button(state, robot)
+#     rc1.on_blue_down = lambda state: right_motor_backward_button(state, robot)
+#
+#     rc2.on_red_up = lambda state: handle_arm_up_button(state, robot)
+#     rc2.on_red_down = lambda state: handle_arm_down_button(state, robot)
+#     rc2.on_blue_up = lambda state: handle_calibrate_button(state, robot)
+#     rc2.on_blue_down = lambda state: handle_calibrate_button(state, robot)
+#
+#     while True:
+#         rc1.process()
+#         rc2.process()
+#         time.sleep(0.1)
+#
+#
+# def left_motor_forward_button(button_state, robot):
+#     if button_state:
+#         robot.left_motor_forward()
+#     else:
+#         robot.left_motor.stop()
+#
+# def left_motor_backward_button(button_state, robot):
+#     if button_state:
+#         robot.left_motor_backward()
+#     else:
+#         robot.left_motor.stop()
+#
+# def right_motor_forward_button(button_state, robot):
+#     if button_state:
+#         robot.right_motor_forward()
+#     else:
+#         robot.right_motor.stop()
+#
+# def right_motor_backward_button(button_state, robot):
+#     if button_state:
+#         robot.right_motor_backward()
+#     else:
+#         robot.right_motor.stop()
+#
+# def handle_arm_up_button(button_state, robot):
+#     if button_state:
+#         robot.arm_up()
+#
+# def handle_arm_down_button(button_state, robot):
+#     if button_state:
+#         robot.arm_down()
+#
+# def handle_calibrate_button(button_state, robot):
+#     if button_state:
+#         robot.arm_calibration()
